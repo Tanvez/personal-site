619eee562888792666ffb33fe4d3ceb342b2faf5
Vesna Tan <tanvezi@gmail.com>
2018-02-22 14:45:26 -0500

    feat: add navbar

--- client/App.js
+++ client/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import './style.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Working?
-      </div>
-    );
+      <header className="header">
+        <h1 className="logo"><a href="#">VT</a></h1>
+        <ul className="main-nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Contact</a></li>
+          </ul>
+      </header>
+    )
   }
 }
 
