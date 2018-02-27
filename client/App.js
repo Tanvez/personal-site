e7376c1fd6e8ae4081e3d8017c6bd51853ee7e84
Vesna Tan <tanvezi@gmail.com>
2018-02-27 17:21:06 -0500

    refactor: file structure

--- client/App.js
+++ client/App.js
@@ -7,36 +7,40 @@ import NavBar from './component/navbar'
 class App extends Component {
   render() {
     return (
-      <div style={{position: 'fixed', height: '100%', width: '100%'}}>
-  <div className="arc_reactor">
-    <div className="case_container">
-      <div className="e7">
-        <div className="semi_arc_3 e5_1">
-          <div className="semi_arc_3 e5_2">
-            <div className="semi_arc_3 e5_3">
-              <div className="semi_arc_3 e5_4">
+
+<div>    
+  <NavBar/>  
+    <div style={{position: 'fixed', height: '100%', width: '100%'}}>
+    <div className="arc_reactor">
+        <div className="case_container">
+          <div className="e7">
+            <div className="semi_arc_3 e5_1">
+              <div className="semi_arc_3 e5_2">
+                <div className="semi_arc_3 e5_3">
+                  <div className="semi_arc_3 e5_4">
+                  </div>
+                </div>
               </div>
             </div>
+            <div className="core2"></div>
           </div>
+          <ul className="marks">
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+            <li></li><li></li><li></li><li></li><li></li><li></li>
+          </ul>
         </div>
-        <div className="core2"></div>
       </div>
-      <ul className="marks">
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-        <li></li><li></li><li></li><li></li><li></li><li></li>
-      </ul>
-    </div>
-  </div>
-</div> 
-    )
+    </div> 
+</div>
+  )
   }
 }
 
--- client/component/navbar/index.js
+++ client/component/navbar/index.js
@@ -5,12 +5,12 @@ class NavBar extends PureComponent {
   render() {
     return (
       <header className="header">
-        <h1 className="logo"><a href="#">VT</a></h1>
+        <h1 className="logo"><a>VT</a></h1>
         <ul className="main-nav">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
+          <li><a>Portfolio</a></li>
+          <li><a>Contact</a></li>
         </ul>
       </header>
     );
