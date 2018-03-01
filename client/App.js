67819a74c6aaedffa334576f52193e386b69714a
Vesna Tan <tanvezi@gmail.com>
2018-03-01 15:27:21 -0500

    feat: add tron nav bar

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import './style.css'
 
@@ -6,42 +7,46 @@ import NavBar from './component/navbar'
 // import border from './container/border'
 
 class App extends Component {
+
   render() {
     return (
       <div>
-      <nav className="draw">
+        <nav className="draw">
           <NavBar />
-      </nav>
-      <div style={{position: 'fixed', height: '100%', width: '100%'}}>
-    <div className="arc_reactor">
-        <div className="case_container">
-          <div className="e7">
-            <div className="semi_arc_3 e5_1">
-              <div className="semi_arc_3 e5_2">
-                <div className="semi_arc_3 e5_3">
-                  <div className="semi_arc_3 e5_4">
+        </nav> 
+
+        <div style={{position: 'fixed', height: '100%', width: '100%'}}>
+                <div className="arc_reactor">
+                <div className="case_container">
+                  <div className="e7">
+                    <div className="semi_arc_3 e5_1">
+                      <div className="semi_arc_3 e5_2">
+                        <div className="semi_arc_3 e5_3">
+                          <div className="semi_arc_3 e5_4">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="core2"></div>
                   </div>
+                  <ul className="marks">
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                    <li></li><li></li><li></li><li></li><li></li><li></li>
+                  </ul>
                 </div>
               </div>
-            </div>
-            <div className="core2"></div>
-          </div>
-          <ul className="marks">
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-            <li></li><li></li><li></li><li></li><li></li><li></li>
-          </ul>
-        </div>
+            </div> 
       </div>
-    </div> 
-      </div>
+      
+
     )
   }
 }
@@ -51,4 +56,3 @@ App.propTypes = {
 }
 
 export default App
-
