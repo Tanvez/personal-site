0f55d30132ad34a5d7851fb1553c9fd4b1fb6af1
Vesna Tan <tanvezi@gmail.com>
2018-03-05 23:25:03 -0500

    feat: add bio

--- client/App.js
+++ client/App.js
@@ -15,36 +15,19 @@ class App extends Component {
         <nav className="draw">
           <NavBar />
         </nav>
-
-        <div style={{position: 'fixed', height: '100%', width: '100%'}}>
-                <div className="arc_reactor">
-                <div className="case_container">
-                  <div className="e7">
-                    <div className="semi_arc_3 e5_1">
-                      <div className="semi_arc_3 e5_2">
-                        <div className="semi_arc_3 e5_3">
-                          <div className="semi_arc_3 e5_4">
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="core2"></div>
-                  </div>
-                  <ul className="marks">
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+        <p className="draw">
+          <p >
+          <h1> BIO </h1>
+          I'm a fullstack software developer who is passionate about producing intuitive user experience.
+          </p>
+          <p>During my final semester in college, I took an intro to computer science and from that first "for" loop, everything clicked for me.</p>
+          <p>
+          In my previous accounting role I've was seen as a technical person. I would train new interns and employees on all types of accounting applications. I would talk to vendors and troubleshoot software. From these experiences I realized that software development is where my passion lies so I applied to Fullstack to deepen my technical knowledge. And going forward I’m looking for a software development role building a consumer facing product.
+          </p>
+       </p>
+       <div>
+       <p className="draw">INSERT OTHER STUFF </p>
+       </div>
       </div>
 
 
@@ -57,3 +40,34 @@ App.propTypes = {
 }
 
 export default App
+
+
+// <div style={{position: 'fixed', height: '100%', width: '100%'}}>
+// <div className="arc_reactor">
+// <div className="case_container">
+//   <div className="e7">
+//     <div className="semi_arc_3 e5_1">
+//       <div className="semi_arc_3 e5_2">
+//         <div className="semi_arc_3 e5_3">
+//           <div className="semi_arc_3 e5_4">
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     <div className="core2"></div>
+//   </div>
+//   <ul className="marks">
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//     <li></li><li></li><li></li><li></li><li></li><li></li>
+//   </ul>
+// </div>
+// </div>
+// </div>
