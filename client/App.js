d6d0581c8463ae543168b4cab801b4eeae53f1e1
Vesna Tan <tanvezi@gmail.com>
2018-03-14 23:04:22 -0400

    feat: added three and expanding borders

--- client/App.js
+++ client/App.js
@@ -5,6 +5,7 @@ import './style.css'
 import '../public/fonts/stylesheet.css'
 
 import NavBar from './component/navbar'
+import Scene from './component/ball'
 // import border from './container/border'
 
 class App extends Component {
@@ -17,19 +18,23 @@ class App extends Component {
         </nav>
         <p className="animate draw">
           <p >
-          <h1> BIO </h1>
-          I'm a fullstack software developer who is passionate about producing intuitive user experience.
+            <h1> BIO </h1>
+            I'm a fullstack software developer
+            who is passionate about producing intuitive user experience.
           </p>
           <p>During my final semester in college, I took an intro to computer science and from that first "for" loop, everything clicked for me.</p>
           <p>
           In my previous accounting role I've was seen as a technical person. I would train new interns and employees on all types of accounting applications. I would talk to vendors and troubleshoot software. From these experiences I realized that software development is where my passion lies so I applied to Fullstack to deepen my technical knowledge. And going forward I’m looking for a software development role building a consumer facing product.
           </p>
        </p>
-       <iframe className="animate draw" width="422" height="240" src="https://www.youtube.com/embed/zDiktgccGjg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe className="test-box" width="422" height="240" src="https://www.youtube.com/embed/zDiktgccGjg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe className="test-box" width="422" height="240" src="https://www.youtube.com/embed/zDiktgccGjg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe className="test-box" width="422" height="240" src="https://www.youtube.com/embed/zDiktgccGjg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
        <div>
        <p className="animate draw"> 
-       
           <p>INSERT OTHER STUFF </p>
+          <Scene />
+          
        </p>
        </div>
       </div>
